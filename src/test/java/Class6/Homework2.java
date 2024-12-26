00d7c7c62ddaa0bdfe768485fc5211d08c1cfca5
Anita Cruz <ntcrz@hotmail.com>
2024-12-26 18:07:34 -0500

    homework 2

--- src/test/java/Class6/Homework2.java
+++ src/test/java/Class6/Homework2.java
@@ -16,6 +16,12 @@ public class Homework2 {
          * Hint: Ternary operator
          */
 
+        int countryName1Length = countryName1.length();
+        System.out.println("\nIf country name length is greater or equal to 6 result1 is 6 otherwise it's 20");
+        System.out.println("Country name is: " +countryName1);
+        result1 = countryName1Length >= 6 ? 6 : 20;
+        System.out.println("The value of result1 is: " +result1);
+
         String countryName2 = "UNiTeD KInGDom OF GReAt BRiTaIn And NorTHerN irElaND";
         /**
          * Ignore-cases for all below tasks
@@ -25,8 +31,16 @@ public class Homework2 {
          * 3. find if countryName2 and countryName1 are having same value
          * 4. replace "D" in countryName2 with "SiGMa"
          */
-
-
-
+        String countryName2LowerCase = countryName2.toLowerCase();
+        System.out.println("\ncountryName2 -> " + countryName2);
+        System.out.println("countryName1 -> " + countryName1);
+        boolean isContains_R= countryName2LowerCase.contains("r");
+        System.out.println("does countryName2 contains 'R' -> " + isContains_R);
+        boolean isStartsWith_ki= countryName2LowerCase.startsWith("ki");
+        System.out.println("does countryName2 starts with 'Ki' -> " + isStartsWith_ki);
+        boolean isCountryName2EqualsIgnoreCase_countryName2 =  countryName2.equalsIgnoreCase(countryName2LowerCase);
+        System.out.println("countryName2 and countryName1 are having same value -> " + isCountryName2EqualsIgnoreCase_countryName2);
+        String replace_D_SiGMa = countryName2.replace("D", "SiGMa");
+        System.out.println("countryName2 after replace 'D' with 'SigMa' -> " + replace_D_SiGMa);
     }
 }
