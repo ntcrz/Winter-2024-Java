85b4edca26558f90eff33dd01a6a32889bcf766b
Anita Cruz <ntcrz@hotmail.com>
2025-02-12 20:02:31 -0500

    Class17

---
+++ src/test/java/Misc/Homework9.java
@@ -0,0 +1,25 @@
+package Misc;
+
+public class Homework9 {
+    public static void main(String[] args) {
+
+        /**
+         * Find the count of color-names in Array
+         */
+        String[] colors = {"green", "red", "green" , "blue", "green", "green", "blue", "orange", "red", "blue", "green"};
+        /*
+            example: {(green-3) , (red-4), (blue-4)}
+         */
+
+        /**
+         * Find the duplicate names from given Array
+         */
+        String[] names = {"john", "liya", "kendra" , "john", "doe", "jay", "riya", "rimmi", "liya"};
+        /*
+            example: [john, liya]
+         */
+
+
+
+    }
+}
